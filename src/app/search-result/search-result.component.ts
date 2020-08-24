32f32c1e4c1a9e60d8f72a00d3f60a6a39a1d9dd
Animesh Raj <rajanimesh64@gmail.com>
2020-08-24 20:27:23 +0530

    hhshshsjjj

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,8 @@
+import { FetchedFlightsDto } from './../fetched-flights-dto';
+import { SearchFlightsService } from './../search-flights.service';
 import { Component, OnInit } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
+import { SearchFlightDto } from '../search-flight-dto';
 
 @Component({
   selector: 'search-result',
@@ -8,20 +12,41 @@ import { Component, OnInit } from '@angular/core';
 export class SearchResultComponent implements OnInit {
 
   searchDetails: SearchDetails[];
-  flights: Flight[];
+  // flights: Flight[];
+  //search: SearchComponent;
 
-  constructor() { }
+  fetchedflights: FetchedFlightsDto = new FetchedFlightsDto;
+  searchdto: SearchFlightDto = new SearchFlightDto;
+  isEconomy: boolean = false;
+  isBusiness: boolean = false;
+
+
+  constructor(private searchservice: SearchFlightsService) { }
 
   ngOnInit(): void {
+    //alert(JSON.stringify(this.search.getFlights()));
+    this.searchdto.source = sessionStorage.getItem('source');
+    this.searchdto.destination = sessionStorage.getItem('destination');
+    this.searchdto.depart = sessionStorage.getItem('depart');
+    this.searchdto.arrive = sessionStorage.getItem('arrive');
+    this.searchdto.noOfPassengers = Number(sessionStorage.getItem('noOfPassengers'));
+    this.searchdto.fclass = sessionStorage.getItem('fclass');
+
+    if(this.searchdto.fclass == "Business")
+      this.isBusiness = true;
+    else if(this.searchdto.fclass == "Economy")
+      this.isEconomy = true;
+
+
+    alert(JSON.stringify(this.searchdto));
+
+    this.searchservice.searchFlights(this.searchdto).subscribe(data => {
+      //alert(JSON.stringify(data));
+      this.fetchedflights = data;
+      alert(JSON.stringify(this.fetchedflights));
+    })
     this.searchDetails = [
-      new SearchDetails("Delhi", "Mumbai", new Date()),
-      new SearchDetails("Mumbai", "Delhi", new Date())
-    ];
-    this.flights = [
-      new Flight("AI 887", "06:50" , "09:00", 5097),
-      new Flight("AI 1002", "16:15" , "18:25", 6000),
-      new Flight("AI 537", "17:00" , "19:15", 4580),
-      new Flight("AI 24", "18:00" , "20:15", 6500)
+      new SearchDetails("Delhi", "Mumbai", new Date())
     ];
   }
 
@@ -37,19 +62,4 @@ class SearchDetails {
     this.to = to;
     this.date = date;
   }
-}
-
-class Flight {
-  id: String;
-  depart: String;
-  arrival: String;
-  price: number;
-
-  constructor(id: String, depart: String, arrival: String, price: number) {
-    this.id = id;
-    this.depart = depart;
-    this.arrival = arrival;
-    this.price = price;
-  }
-
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { SearchFlightDto } from './../search-flight-dto';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,10 +9,10 @@ import { Router } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
-  search: Search = new Search();
+  public searchdto: SearchFlightDto = new SearchFlightDto;
 
   public City = [{ name: 'Agartala' }, { name: 'Agra'}, { name: 'Ahmedabad'},{ name: 'Aizwal' }, { name: 'Allahabad'}, { name: 'Amritsar'}, { name: 'Aurangabad' }, { name: 'Banglore'}, { name: 'Bhopal'}, { name: 'Bhubaneshwar' }, { name: 'Chandigarh'}, { name: 'Chennai'}, { name: 'Coimbatore' }, { name: 'Darjeeling'}, { name: 'Dehradun'}, { name: 'Delhi' }, { name: 'Dharamsala'}, { name: 'Gandhinagar'}, { name: 'Goa' }, { name: 'Guwhati'}, { name: 'Hyderabad'}, { name: 'Imphal' }, { name: 'Indore'}, { name: 'Jammu'}, { name: 'Kanpur' }, { name: 'Kochi'}, { name: 'Kolkata'}, { name: 'Kozhikode' }, { name: 'Lucknow'}, { name: 'Mumbai'}, { name: 'Nagpur' }, { name: 'Nasik'}, { name: 'Patna'},{ name: 'Pune' }, { name: 'Raipur'}, { name: 'Rajkot'}, { name: 'Ranchi' }, { name: 'Shillong'}, { name: 'Shimla'},{ name: 'Srinagar' }, { name: 'Tiruvanantpuram'}, { name: 'Vishakhapatnam'} ];
-
+  public passengers:number[] = [1,2,3,4,5];
   public selected: boolean = true;
   public today = new Date();
   // public departDate = this.today.getFullYear() + "-" + (((this.today.getMonth()+1)<10?'0':'') + (this.today.getMonth()+1)) + "-" + (this.today.getDate()<10?'0':'' + this.today.getDate());
@@ -25,11 +26,17 @@ export class SearchComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    sessionStorage.setItem('source', null);
+    sessionStorage.setItem('destination', null);
+    sessionStorage.setItem('depart', null);
+    sessionStorage.setItem('arrive', null);
+    sessionStorage.setItem('noOfPassengers', null);
+    sessionStorage.setItem('fclass', null);
   }
 
-  // isSelected() {
-    
-  // }
+  indexTracker(index: number, value: any) {
+    return index;
+  }
 
   // radio button enable-disable return date entry
   onChangeTrip(e) {
@@ -55,13 +62,15 @@ export class SearchComponent implements OnInit {
   }
 
   searchFlights() {
-    alert("Form Submitted")
+    alert(JSON.stringify(this.searchdto));
+    sessionStorage.setItem('source', this.searchdto.source);
+    sessionStorage.setItem('destination', this.searchdto.destination);
+    sessionStorage.setItem('depart', this.searchdto.depart);
+    sessionStorage.setItem('arrive', this.searchdto.arrive);
+    sessionStorage.setItem('noOfPassengers', this.searchdto.noOfPassengers.toString());
+    sessionStorage.setItem('fclass', this.searchdto.fclass);
     this.router.navigate(['search-result']);
   }
 
-}
 
-class Search{
-  fromCity: string;
-  toCity: string;
 }
