5b749dda590d937dab76ba76ff015a22ea09296f
Animesh Raj <rajanimesh64@gmail.com>
2020-08-27 14:44:51 +0530

    view all booking details

---
+++ src/app/view-booking-dto.ts
@@ -0,0 +1,12 @@
+export class ViewBookingDto {
+    id: number;
+	bookingDateTime: any;
+	totalPassengers: number;
+	bookingAmount: any;
+	status: string;
+	depart:any;
+	arrive: any;
+	source: string;
+	destination: string;
+
+}
